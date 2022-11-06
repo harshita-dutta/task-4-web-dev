7f86cfab030a7b6da8dd2d5fe46d52063a8a764d
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-11-06 08:25:19 +0530

    Random ID + One time form implemented

--- src/js/firebaseUtils.js
+++ src/js/firebaseUtils.js
@@ -4,10 +4,10 @@ import 'bootstrap'
 import  "./ajaxUtils"
 import "jquery"
 import { initializeApp } from 'firebase/app'
-import {collection,doc,getDoc,getDocs,getFirestore,onSnapshot,query,where} from 'firebase/firestore'
+import { doc, setDoc, getFirestore} from 'firebase/firestore'
 import { createUserWithEmailAndPassword,signInWithPopup, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import { createAlert } from "./ajaxUtils"
-
+import { getDatabase } from "firebase/database";
 
 // Add your config here
 const firebaseConfig = {
@@ -24,103 +24,159 @@ initializeApp(firebaseConfig);
 const auth = getAuth();
 
 
+// CountDown Timer:
+var countDownDate = new Date("Dec 2, 2022 09:00:00").getTime();
+var countdown = function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  var timers = document.querySelectorAll(".countdown");
+  timers.forEach(element => {
+    element.innerHTML = "Time Remaining: "+
+    days + "d " + hours + "hr(s) " + minutes + "min(s) " + seconds + "sec ";
+  if (distance < 0) {
+    clearInterval(x);
+    element.innerHTML = "Event Expired";
+  }
+  });
+  
+}
+var x = setInterval(countdown, 1000);
+
+
+
+// Checking Login Status to allow the user
+document.addEventListener("DOMContentLoaded",()=>{
+  onAuthStateChanged(auth, (user) => {
+    const user1 = auth.currentUser;
+    if(user){
+      if(user.displayName){
+        document.getElementById("logout").textContent = user1.displayName;
+      }
+      else{
+        user1.displayName = window.localStorage.username;
+        document.getElementById("logout").textContent = user1.displayName;
+      }
+      window.localStorage.uid = user1.uid;
+
+      // Form Verification:
+      const db = getFirestore();
+      const docRef = doc(db,"Reg",user.uid);
+      if(docRef){
+        const buttons = document.querySelectorAll(".button");
+        buttons.forEach(button => {
+          button.textContent = "View Pass";
+          button.setAttribute('href','passpage.html');
+        });
+
+      // Random ID on Screen
+      let tokenDiv = document.querySelector(".token-number");
+      if(tokenDiv){
+          // Change token ID here
+      }
+      }
+    }
+  })
+})
+
 
 // Logging out of your profile
 const logoutButton = document.querySelector('#logout')
-logoutButton.addEventListener('click', () => {
-  signOut(auth)
-    .then(() => {
-      alert("Logged Out");
-      document.location.reload();
-      document.location.href = "index.html";
-      
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-})
+if(logoutButton){
+  logoutButton.addEventListener('click', () => {
+    signOut(auth)
+      .then(() => {
+        alert("Logged Out");
+        document.location.reload();
+        document.location.href = "index.html";
+        
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+  })
+}
 
 // Sign Up Form Event
 const signupform = document.querySelector('.signup');
+if(signupform){
 signupform.addEventListener('submit',(e)=>{
     e.preventDefault();
     const email = signupform.Email1.value;
     const password = signupform.Password1.value;
-    console.log(email,password);
+    const username = signupform.username.value;
+    console.log(username,email,password);
+    window.localStorage.username = username;
     createUserWithEmailAndPassword(auth,email,password)
         .then((cred)=>{
-          console.log(cred)
-          signInWithEmailAndPassword(auth, email, password)
-          .then(cred => {
-              document.location.href = "mainpage.html";
-          })
-          .catch(err => {
-            console.log(err.message)
-          })
           signupform.reset(); 
+          document.location.href = "mainpage.html";
+          
         })
         .catch((err)=>{
             createAlert(err.message,"alert alert-danger");
         })
 })
+}
 
 // Sign In Form Event
 const loginForm = document.querySelector('.login')
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault()
+if(loginForm){
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-  const email = loginForm.Email1.value
-  const password = loginForm.Password1.value
+    const email = loginForm.Email1.value
+    const password = loginForm.Password1.value
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then(cred => {
-      
-      console.log(cred.user);
-      loginForm.reset();
-      document.location.href = "mainpage.html";
+    signInWithEmailAndPassword(auth, email, password)
+      .then(cred => {
+        
+        console.log(cred.user);
+        loginForm.reset();
+        document.location.href = "mainpage.html";
 
-    })
-    .catch(err => {
-      createAlert(err.message,"alert alert-danger");
-    })
-})
-
+      })
+      .catch(err => {
+        createAlert(err.message,"alert alert-danger");
+      })
+  })
+}
 // Sign In Using Google
 const provider = new GoogleAuthProvider();
 const logGoogle = document.getElementById("logGoogle");
-logGoogle.addEventListener("click", 
-    ()=>{
-      signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    document.getElementById("logout").textContent = user;
-    console.log(user);
-    document.location.href = "mainpage.html";
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-  });
-    }
-)
+if(logGoogle){
+  logGoogle.addEventListener("click", 
+      ()=>{
+        signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      document.getElementById("logout").textContent = user;
+      console.log(user);
+      document.location.href = "mainpage.html";
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+    });
+      }
+  )
 
-// Checking Login Status to allow the user
-onAuthStateChanged(auth, (user) => {
-  if(user!=null){
-    console.log(user);
-   
-    
-  }
-})
+}
 
 
 // Sign In and Sign Up toggle 
